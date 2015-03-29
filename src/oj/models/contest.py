6e280764501990cb14a448a582ce0f515ea9fb04
zrq495 <zrq495@gmail.com>
2015-03-29 13:38:26 +0800

    update contest models

--- src/oj/models/contest.py
+++ src/oj/models/contest.py
@@ -2,6 +2,9 @@
 
 from __future__ import unicode_literals
 
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import sql
+
 from oj import db
 
 
@@ -16,10 +19,31 @@ class ContestModel(db.Model):
     __tablename__ = 'contest'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(512), nullable=False)
+    name = db.Column(db.Unicode(512), nullable=False)
+    date_start = db.Column(db.DateTime, nullable=False)
+    date_end = db.Column(db.DateTime, nullable=False)
+    description = db.Column(db.UnicodeText)
+    type = db.Column(
+        db.Enum('public', 'private', 'register', 'diy',
+                name='contest_type_enum'),
+        server_default='public', nullable=False)
+    user_id = db.Column(db.Integer(), nullable=False)
+    password_hash = db.Column(db.String(128))
+    date_start_register = db.Column(db.DateTime, nullable=False)
+    date_end_register = db.Column(db.DateTime, nullable=False)
+    is_hiden = db.Column(
+        db.Boolean, default=False, server_default=sql.false(),
+        nullable=True)
     date_created = db.Column(
         db.DateTime, nullable=False,
         server_default=db.func.current_timestamp())
+
+    user = db.relationship(
+        'UserModel',
+        primaryjoin='UserModel.id==ContestModel.user_id',
+        foreign_keys='[ContestModel.user_id]',
+        lazy=True
+    )
 
     solutions = db.relationship(
         'SolutionModel',
@@ -41,8 +65,7 @@ class ContestModel(db.Model):
         foreign_keys='[ContestProblemModel.contest_id]',
         backref=db.backref(
             'contest',
-            lazy='joined',
-            innerjoin=True),
+            lazy=True),
         order_by='ContestProblemModel.ordinal.asc()',
         passive_deletes='all',
         lazy='dynamic'
@@ -57,30 +80,42 @@ class ContestModel(db.Model):
         seed()
         for i in range(count):
             c = ContestModel(
-                title=forgery_py.lorem_ipsum.title())
+                name=forgery_py.lorem_ipsum.name())
             db.session.add(c)
             try:
                 db.session.commit()
             except IntegrityError:
                 db.session.rollback()
 
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def as_dict(self):
         return {
             'id': self.id,
-            'title': self.title,
+            'name': self.name,
         }
 
     def __repr__(self):
-        return '<Contest %r>' % self.title
+        return '<Contest %r>' % self.name
 
 
 class ContestProblemModel(db.Model):
     __tablename__ = 'contest_problem'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(512))
     contest_id = db.Column(db.Integer(), nullable=False)
     problem_id = db.Column(db.Integer(), nullable=False)
-    ordinal = db.Column(db.Integer(), default=0)
+    ordinal = db.Column(db.Integer(), default=0, server_default='0')
     date_created = db.Column(
         db.DateTime, nullable=False,
         server_default=db.func.current_timestamp())
@@ -89,7 +124,7 @@ class ContestProblemModel(db.Model):
         'ProblemModel',
         primaryjoin='ProblemModel.id==ContestProblemModel.problem_id',
         foreign_keys='[ContestProblemModel.problem_id]',
-        uselist=False
+        lazy=True
     )
 
     @staticmethod
@@ -105,8 +140,10 @@ class ContestProblemModel(db.Model):
         problem_count = ProblemModel.query.count()
         if contest_count == 0:
             ContestModel.generate_fake()
+            contest_count = ContestModel.query.count()
         if problem_count == 0:
             ProblemModel.generate_fake()
+            problem_count = ProblemModel.query.count()
         for i in range(count):
             c = ContestModel.query.offset(randint(0, contest_count - 1)).first()
             p = ProblemModel.query.offset(randint(0, problem_count - 1)).first()
@@ -134,9 +171,55 @@ class ContestUserModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     contest_id = db.Column(db.Integer(), nullable=False)
+    login_name = db.Column(db.String(256), nullable=False)
+    team_name = db.Column(db.Unicode(256), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    status = db.Column(
+        db.Enum(
+            'accept', 'waiting', 'reject', 'again',
+            name='contest_user_status_enum'),
+        server_default='waiting', nullable=False)
+    seat_number = db.Column(db.Unicode(128))
+    reason = db.Column(db.Unicode(256))
+    user_id = db.Column(db.Integer(), nullable=False)
+    student_id = db.Column(db.String(32))
+    name = db.Column(db.Unicode(256))
+    school = db.Column(db.Unicode(256))
+    college = db.Column(db.Unicode(256))
+    major = db.Column(db.Unicode(256))
+    clazz = db.Column(db.Unicode(256))
+    phone = db.Column(db.String(32))
+    email = db.Column(db.String(256))
+    student_id1 = db.Column(db.String(32))
+    name1 = db.Column(db.Unicode(256))
+    school1 = db.Column(db.Unicode(256))
+    college1 = db.Column(db.Unicode(256))
+    major1 = db.Column(db.Unicode(256))
+    clazz1 = db.Column(db.Unicode(256))
+    phone1 = db.Column(db.String(32))
+    email1 = db.Column(db.String(256))
+    student_id2 = db.Column(db.String(32))
+    name2 = db.Column(db.Unicode(256))
+    school2 = db.Column(db.Unicode(256))
+    college2 = db.Column(db.Unicode(256))
+    major2 = db.Column(db.Unicode(256))
+    clazz2 = db.Column(db.Unicode(256))
+    phone2 = db.Column(db.String(32))
+    email2 = db.Column(db.String(256))
     date_created = db.Column(
         db.DateTime, nullable=False,
         server_default=db.func.current_timestamp())
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def as_dict(self):
         return {
