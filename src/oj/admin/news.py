b5dafb9cf6ead7e580c9f45e773ad953c46882b0
zrq495 <zrq495@gmail.com>
2015-04-26 21:49:20 +0800

    add headline to admin

--- src/oj/admin/news.py
+++ src/oj/admin/news.py
@@ -3,8 +3,40 @@
 from __future__ import unicode_literals
 
 from oj import db
-from oj.models import NewsModel
+from oj.models import NewsModel, HeadlineModel
 from .mixin import Mixin
 from . import flask_admin
 
-flask_admin.add_view(Mixin(NewsModel, db.session))
+
+class NewsAdmin(Mixin):
+
+    column_exclude_list = ['content']
+    column_searchable_list = ['title', 'content']
+    column_filters = [
+        'id', 'user_id', 'title', 'is_display', 'date_created'
+    ]
+
+    form_excluded_columns = ['user_id', 'date_created']
+
+    def __init__(self, session, **kwargs):
+        super(NewsAdmin, self).__init__(NewsModel, session, **kwargs)
+
+
+class HeadlineAdmin(Mixin):
+
+    column_exclude_list = ['url']
+    column_searchable_list = ['title', 'url', 'alert']
+    column_filters = [
+        'id', 'user_id', 'title', 'url', 'alert', 'is_display', 'date_created'
+    ]
+
+    form_excluded_columns = ['user_id', 'date_created']
+
+    def __init__(self, session, **kwargs):
+        super(HeadlineAdmin, self).__init__(HeadlineModel, session, **kwargs)
+
+
+flask_admin.add_view(NewsAdmin(
+    db.session, name='新闻列表', category='新闻管理', url='news'))
+flask_admin.add_view(HeadlineAdmin(
+    db.session, name='滚动条列表', category='新闻管理', url='headline'))
