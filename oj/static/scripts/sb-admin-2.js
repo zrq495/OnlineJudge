b648019382cd3dcc6d312736966222c10782fd30
nplus <junnplus@qq.com>
2015-06-01 17:00:22 +0800

    nav active

--- oj/static/scripts/sb-admin-2.js
+++ oj/static/scripts/sb-admin-2.js
@@ -30,7 +30,9 @@ $(function() {
     var element = $('ul.nav a').filter(function() {
         return this.href == url || url.href.indexOf(this.href) == 0;
     }).addClass('active').parent().parent().addClass('in').parent();
+    /*
     if (element.is('li')) {
         element.addClass('active');
     }
+    */
 });
